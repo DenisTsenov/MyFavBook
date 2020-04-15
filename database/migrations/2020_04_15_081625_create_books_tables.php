df3a4353d1ff75f0527abbaafaecb883562ed162
DenisTSenov <denis_cenov_@abv.bg>
2020-04-15 11:46:53 +0300

    add base data structure

---
+++ database/migrations/2020_04_15_081625_create_books_tables.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('isbn');
+            $table->string('description');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+
+            $table->unique(['book_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_user');
+        Schema::dropIfExists('books');
+    }
+}
